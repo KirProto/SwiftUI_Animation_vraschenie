0fd0c38567225c3370ee31419cfda2bea9a2bdf0
Kir Proto Q <kir.proto@ya.ru>
2022-02-11 09:29:41 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Animation_vraschenie/Yan_SwiftUI_Animation_vraschenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Animation_vraschenieApp.swift
+//  Yan_SwiftUI_Animation_vraschenie
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Animation_vraschenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
